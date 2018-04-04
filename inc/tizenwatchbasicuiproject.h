336c8ac5e8f9b190b98cb6e842b0b6486926d9a2
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-04-04 17:24:38 +0800

    initial commit

---
+++ inc/tizenwatchbasicuiproject.h
@@ -0,0 +1,19 @@
+#ifndef __tizenwatchbasicuiproject_H__
+#define __tizenwatchbasicuiproject_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "tizenwatchbasicuiproject"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.freeankit.tizenwatchbasicuiproject"
+#endif
+
+#endif /* __tizenwatchbasicuiproject_H__ */
